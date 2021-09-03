bb61d2f86219e337916d6203c6c49b0d755e7272
Dang Minh Truong <truongdm4@ghtk.co>
2021-09-03 22:11:27 +0700

    Basic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import ErrorBoundary from './ErrorBoundary';
+import BuggyCounter from './BuggyCounter';
+import * as Sentry from "@sentry/react";
+
+function FallbackComponent() {
+  return <div>An error has occurred</div>;
+}
+
+const myFallback = <FallbackComponent />;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ErrorBoundary>
+    {/* <Sentry.ErrorBoundary fallback={<p>An error has occurred</p>} showDialog> */}
+      <div>
+        <p>The counter is programmed to throw when it reaches 5. This simulates a JavaScript error in a component.</p>
+        <BuggyCounter />
+      </div>
+    {/* </Sentry.ErrorBoundary> */}
+    </ErrorBoundary>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+Sentry.init({
+  dsn: "https://ccd8dc39fdd54ed6915f5e7ea2449bbc@o354753.ingest.sentry.io/5944340",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
